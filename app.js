a9ea18b1430b5cae5bf359e986b86863213a1ac2
Ömer Can <omercanhocaoglu@gmail.com>
2024-04-17 15:45:58 +0300

    ejs dosyaları oluşturuldu.

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 
 const app = express();
+// Template engine
+app.set("view engine", "ejs");
+// Middlewares
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.status(200).send(`<h1> Hello World! </h1>`);
+  res.status(200).render('index', {
+    page_name: "index"
+  });
+});
+app.get("/about", (req, res) => {
+  res.status(200).render('about', {
+    page_name: "about"
+  });
 });
 
 const port = 3000;
