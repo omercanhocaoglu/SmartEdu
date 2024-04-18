7172cf121608b9dcdcb0a21ee2a4d8634028b107
Ömer Can <omercanhocaoglu@gmail.com>
2024-04-18 15:52:24 +0300

    Models ve controller oluşturuldu.

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
 
 const app = express();
 // Template engine
 app.set("view engine", "ejs");
 // Middlewares
 app.use(express.static("public"));
+// Connect DB
+mongoose.connect('mongodb://localhost/smartedu-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(() => {
+  console.log('db connected succesfully');
+});
 
-app.get("/", (req, res) => {
-  res.status(200).render('index', {
-    page_name: "index"
-  });
-});
-app.get("/about", (req, res) => {
-  res.status(200).render('about', {
-    page_name: "about"
-  });
-});
+app.use("/", pageRoute);
+app.use("/courses", courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
