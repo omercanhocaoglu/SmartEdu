ce37a6a5e179657d131f92276f11e316558bae8e
Ömer Can <omercanhocaoglu@gmail.com>
2024-04-26 15:34:35 +0300

    user oluşturuldu.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
+const userRoute = require("./routes/userRoute");
 
 const app = express();
 // Template engine
@@ -24,6 +25,7 @@ mongoose.connect('mongodb://localhost/smartedu-db', {
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
 app.use('/categories', categoryRoute);
+app.use('/users', userRoute);
 
 const port = 3000;
 app.listen(port, () => {
