9b0ae57d2decabcceca2a1110f6a3b4f2fa09497
Ömer Can <omercanhocaoglu@gmail.com>
2024-05-01 13:24:18 +0300

    dashboard oluşturuldu.

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,7 +24,7 @@ exports.loginUser = async (req, res) => {
         bcrypt.compare(password, user.password, (err, same) => {
           if (same) {
             req.session.userID = user._id;
-            res.status(200).redirect("/");
+            res.status(200).redirect("/users/dashboard");
           }
         });
       }
@@ -39,5 +39,12 @@ exports.loginUser = async (req, res) => {
 exports.logoutUser = (req, res) => {
   req.session.destroy(() => {
     res.redirect("/");
+  });
+};
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({_id: req.session.userID})
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user
   });
 };
