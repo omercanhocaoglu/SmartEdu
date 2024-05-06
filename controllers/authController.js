53b807e1230d66d880e106087d0213e03b7af126
Ömer Can <omercanhocaoglu@gmail.com>
2024-05-06 15:13:21 +0300

    modal çalışmaları oluşturuldu.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const Category = require("../models/Category");
 const bcrypt = require("bcrypt");
 const session = require("express-session");
 
@@ -39,9 +40,11 @@ exports.logoutUser = (req, res) => {
   });
 };
 exports.getDashboardPage = async (req, res) => {
-  const user = await User.findOne({_id: req.session.userID})
+  const user = await User.findOne({_id: req.session.userID});
+  const categories = await Category.find();
   res.status(200).render('dashboard', {
     page_name: 'dashboard',
-    user
+    user,
+    categories
   });
 };
