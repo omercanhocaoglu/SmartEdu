cf52c6fee31ee66eddadd6a9363318425ec2a27d
Ömer Can <omercanhocaoglu@gmail.com>
2024-05-10 15:50:27 +0300

    search işlemleri tamamlandı

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -21,13 +21,26 @@ exports.createCourse = async (req, res) => {
 exports.getAllCourses = async (req, res) => {
   try {
     const categorySlug = req.query.categories;
+    const query = req.query.search;
     const category = await Category.findOne({ slug: categorySlug });
     let filter = {};
     if (categorySlug) {
       filter = { category: category._id };
-    }
+    };
+    if (query) {
+      filter = { name:query };
+    };
+    if (!query && !categorySlug) {
+      filter.name = "";
+      filter.category = null;
+    };
 
-    const courses = await Course.find(filter).sort('-createdAt');
+    const courses = await Course.find({
+      $or: [
+        { name: { $regex: '.*' + filter.name + '.*', $options: 'i' } },
+        {category: filter.category}
+      ]
+    }).sort('-createdAt').populate('user');
     const categories = await Category.find();
     res.status(200).render("courses", {
       courses,
