e64198d10ec95c794d61030a4f4efee5432c57a1
Ömer Can <omercanhocaoglu@gmail.com>
2024-05-11 19:20:33 +0300

    contact dosyaları oluşturuldu

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+const nodemailer = require("nodemailer");
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
   res.status(200).render("index", {
@@ -18,4 +20,45 @@ exports.getLoginPage = (req, res) => {
   res.status(200).render("login", {
     page_name: "login",
   });
+};
+exports.getContactPage = (req, res) => {
+  res.status(200).render("contact", {
+    page_name: "contact",
+  });
+};
+exports.sendEmail = async (req, res) => {
+  const outputMessage = `
+    <h1> Mail Details </h1>
+    <ul>
+      <li> Name: ${req.body.name} </li>
+      <li> Email: ${req.body.email} </li>
+    </ul>
+    <h1> Message </h1>
+    <p> ${req.body.message} </p>
+  `
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "omercanhocaoglu53@gmail.com", // gmail account
+      pass: "yaatocrqqfbidazw", // gmail password
+    },
+  });
+  
+  // async..await is not allowed in global scope, must use a wrapper
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Smart EDU Contact Form" <omercanhocaoglu53@gmail.com>', // sender address
+    to: "omercanhocaoglu@gmail.com", // list of receivers
+    subject: "Smart EDU Contact Form New Message ✔", // Subject line
+    html: outputMessage, // html body
+  });
+    
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  res.status(200).redirect('contact');
 };
