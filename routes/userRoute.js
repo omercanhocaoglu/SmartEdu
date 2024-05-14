20440fc8ef0a8a64a7b46ee6b4a5b00aca3e3ab2
Ömer Can <omercanhocaoglu@gmail.com>
2024-05-14 19:06:41 +0300

    validation işlemleri tamamlandı

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,25 @@
 const express = require("express");
-const authController = require('../controllers/authController');
+const authController = require("../controllers/authController");
 const authMiddleware = require("../middlewares/authMiddleware");
+const { body } = require("express-validator");
+const User = require("../models/User");
 
 const router = express.Router();
-router.route("/signup").post(authController.createUser);
+router.route("/signup").post(
+    [
+        body("name").not().isEmpty().withMessage("Please enter your name"),
+        body("email").isEmail().withMessage("Please enter valid email adress")
+        .custom((userEmail) => {
+            return User.findOne({email:userEmail}).then(user => {
+                if (user) {
+                    return Promise.reject("This email is already exists!");
+                };
+            });
+        }),
+        body("password").not().isEmpty().withMessage("Please enter your password")
+    ],
+
+    authController.createUser);
 router.route("/login").post(authController.loginUser);
 router.route("/logout").get(authController.logoutUser);
 router.route("/dashboard").get(authMiddleware, authController.getDashboardPage);
