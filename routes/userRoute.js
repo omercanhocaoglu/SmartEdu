7bbbbce0f47393f43a1bd437e04d0e4d5d44ea3b
Ömer Can <omercanhocaoglu@gmail.com>
2024-05-17 13:46:41 +0300

    admin işlemleri tamamlandı

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -23,5 +23,6 @@ router.route("/signup").post(
 router.route("/login").post(authController.loginUser);
 router.route("/logout").get(authController.logoutUser);
 router.route("/dashboard").get(authMiddleware, authController.getDashboardPage);
+router.route("/:id").delete(authController.deleteUSer);
 
 module.exports = router;
